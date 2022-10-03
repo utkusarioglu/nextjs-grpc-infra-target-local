12732fc8665bde420937bea69552cbf8f8ef9e6e
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-03 14:16:37 +0000

    Chore(template): Accept template changes
    - Updates and bugfixes in template updaters
    - Refactoring of devcontainer volumes in `docker-compose.yml`.
    - Pruning of `tasks.json`.
    - Tests now recognize `.env.ENVIRONMENT` and choose `tfvars` files
      accordingly.
    - Add `required.template.tf` to repo auto reject list.

--- scripts/git-template-update-adjustments.sh
+++ scripts/git-template-update-adjustments.sh
@@ -2,7 +2,7 @@
 
 source scripts/config.sh
 check_repo_config
-source scripts/git-facades.sh
+source scripts/git-utils.sh
 
 git_origin_update() {
   record_target=$1
@@ -25,15 +25,18 @@ template_auto_reject=$6
 
 current=$(cat .devcontainer/devcontainer.json | jq -r '.name')
 replacement=$repo_class
-find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) -exec sed -i "s:$current:$replacement:g" {} \;
+find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) \
+  -exec sed -i "s:$current:$replacement:g" {} \;
 
 current=$(cat .devcontainer/devcontainer.json | jq -r '.workspaceFolder')
 replacement=$repo_path
-find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) -exec sed -i "s:$current:$replacement:g" {} \;
+find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) \
+  -exec sed -i "s:$current:$replacement:g" {} \;
 
 current=$(cat .devcontainer/devcontainer.json | jq -r '.service')
 replacement=$repo_service
-find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) -exec sed -i "s:$current:$replacement:g" {} \;
+find . -type f \( ! -iwholename "./scripts/setup.sh" ! -iname ".git" \) \
+  -exec sed -i "s:$current:$replacement:g" {} \;
 
 git_template_repo_url_update $record_target $template_repo_url
 
