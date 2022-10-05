6d27a9b0fd676d887789d15ecf9d34b1e0e7f23b
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-05 13:34:25 +0000

    Chore: Provide `app_tier_1` with vars it needs
    - Add `sld` and `tld` among the values provided to the module. This
      change is necessitated by the updates in that particular module.

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 module "app_tier_1" {
-  source            = "../../configs/app/modules/tier-1"
+  source = "../../configs/app/modules/tier-1"
+
   project_root_path = local.project_root_path
   helm_timeout_unit = var.helm_timeout_unit
   helm_atomic       = var.helm_atomic
+  sld               = var.sld
+  tld               = var.tld
 }
 
 module "ingress" {
