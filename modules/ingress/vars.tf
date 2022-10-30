34a5ccbb20988cde1f83780fc2b67f83f433c129
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-30 15:18:56 +0000

    Refactor: Ingress certificate provision by tf
    - Remove ingress certificate creation responsibility from cert-manager
      and use terraform for the purpose instead. This is done because in a
      real cloud environment, the responsiblity for creating the ingress
      certificate belongs to the cloud provider or some global trusted
      entity. cert-manager being mixed up in this provisioning makes the
      terraform config needlessly complicated. And this would get even worse
      with the inclusion of vault in the cluster. Ingress simply doesn't
      need to be involved with that pipeline.
    - Add `.certs` to gitignore. This path will contain the CA for ingress
      cert for local deployment.
    - Refactor the order of creation for app tiers and ingress. Now that
      ingress doesn't need tier 1, `app_tier_1` is moved down the list.
    - Create a new namespace named `ingress` to contain ingress related
      resources.

--- modules/ingress/vars.tf
+++ modules/ingress/vars.tf
@@ -14,3 +14,11 @@ variable "deployment_mode" {
   type        = string
   description = "Specify a mode that determines which resources will be deployed. Example: 'all' deploys everything"
 }
+
+variable "sld" {
+  type = string
+}
+
+variable "tld" {
+  type = string
+}
